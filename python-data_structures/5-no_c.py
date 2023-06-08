90992aac1c4794b2155914f9c1e760ee4dda341f
Pablo <p5.tacoronte@gmail.com>
2023-06-08 11:04:17 -0700

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_dic = {ord('c') : "", ord('C') : ""}
+    my_dic = {ord('c'): "", ord('C'): ""}
     ns = my_string.translate(my_dic)
     return ns
