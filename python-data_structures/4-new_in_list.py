1ce2a062368d7b0830fd49bd88cd214ca95e063b
Pablo <p5.tacoronte@gmail.com>
2023-06-08 09:52:21 -0700

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,10 +2,6 @@
 
 def new_in_list(my_list, idx, element):
     nl = my_list.copy()
-    length = len(nl)
-    if idx > 0:
-        if idx <= length:
-            nl[idx] = element
-        else:
-            nl.append(element)
+    if idx >= 0 and idx < len(my_list):
+        nl[idx] = element
     return nl
