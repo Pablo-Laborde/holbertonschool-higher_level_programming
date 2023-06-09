485bb1de2a30c9fd7c7f20c746640c500f59d823
Pablo <p5.tacoronte@gmail.com>
2023-06-09 15:00:32 -0700

    3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    myset = set()
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                myset.add(i)
+    return myset
