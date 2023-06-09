9066a9294de7f78a9fed6929fa1a95b343426159
Pablo <p5.tacoronte@gmail.com>
2023-06-09 12:10:10 -0700

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    nl = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
+            nl.append(replace)
+        else:
+            nl.append(my_list[i])
     return my_list
