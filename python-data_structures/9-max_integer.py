b920a77ece733552414d0372e2c63f33e4c8f16d
Pablo <p5.tacoronte@gmail.com>
2023-06-09 04:24:38 -0700

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
         for i in range(len(my_list)):
             if my_list[i] > my_list[a]:
                 a = i
-        return a
+        return my_list[a]
     else:
         return None
