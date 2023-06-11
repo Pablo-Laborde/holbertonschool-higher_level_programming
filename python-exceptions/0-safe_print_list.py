d0ab2af88fbe8227fb70172c5e3ef90cf4d107ba
Pablo <p5.tacoronte@gmail.com>
2023-06-11 11:46:33 -0700

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    if my_list and x > 0:
+    if my_list and x >= 0:
         for i in range(x):
             try:
                 print("{}".format(my_list[i]), end="")
