570d6345ba493e3b072c9b17b4f3df734f44fb75
Pablo <p5.tacoronte@gmail.com>
2023-06-11 12:38:36 -0700

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,5 @@ def safe_print_integer(value):
         try:
             print("{:d}".format(value))
             return True
-        except Exception as ex:
+        except Exception:
             return False
