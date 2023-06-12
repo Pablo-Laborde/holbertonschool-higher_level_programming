e0de591ccd0f611c85ce970c7ea10e5a4d8e3764
Pablo <p5.tacoronte@gmail.com>
2023-06-11 18:32:45 -0700

    101

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        return fct(*args)
+    except Exception as ex:
+        stderr.write('Exception: {}\n'.format(ex))
+        return None
