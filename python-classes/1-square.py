5322ea5c2d36beb130de27feda2577dbe266069d
Pablo <p5.tacoronte@gmail.com>
2023-06-14 20:18:19 -0700

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Ex 1 """
+
+class Square:
+    """ Square class """
+    __size = 0
+
+    def __init__(self, size):
+        """ Instantiation square """
+        self.__size = size
