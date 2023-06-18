eb55846250c8cb6d609bcb35f6434e89d47cf23e
Pablo <p5.tacoronte@gmail.com>
2023-06-18 16:40:25 -0700

    2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module Ex 1 """
+
+
+def matrix_divided(matrix, div):
+    for i in matrix:
+        for j in i:
+            if (type(j) != int) and (type(j) != float):
+                raise TypeError("matrix must be a matrix (list of lists)\
+                                of integers/floats")
+    for i in range(len(matrix)):
+        if len(matrix[i]) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if (type(div) != int) and (type(div) != float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return list(map(lambda i: list(map(lambda j: j / div, i)), matrix))
