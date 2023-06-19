30bb75bf1874f889b7d2fcaf53d8aaf7e6e5c6dd
Pablo <p5.tacoronte@gmail.com>
2023-06-18 20:02:20 -0700

    6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,7 +13,3 @@ class TestMaxInt(unittest.TestCase):
         self.assertEqual(max_integer([1, 2]), 2)
         self.assertEqual(max_integer(), None)
         self.assertEqual(max_integer([-2, -3, -4, -8]), -2)
-
-    def test_val(self):
-        """ test fucntion """
-        self.assertRaises(TypeError, max_integer("hola"), 2)
