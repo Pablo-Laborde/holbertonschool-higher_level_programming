b517d69ed77a7f2011f3ee509467b32a38460e24
Pablo <p5.tacoronte@gmail.com>
2023-06-18 18:35:56 -0700

    5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,10 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
-        print(text[i], end="")
-        if text[i] in ['.', '?', ':']:
-            if i < len(text) - 1:
-                print("\n")
+        if (text[i] == ' ') and (text[i - 1] in ['.', '?', ':']):
+            pass
+        else:
+            print(text[i], end="")
+            if text[i] in ['.', '?', ':']:
+                if i < len(text) - 1:
+                    print("\n")
