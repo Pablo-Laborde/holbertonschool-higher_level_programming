01503aa2c51b027886a06cac6b82c725ecdfd490
Pablo <p5.tacoronte@gmail.com>
2023-06-24 21:08:15 -0700

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Ex 6 """
+
+
+class BaseGeometry():
+    """ Basegeometry class """
+    def area(self):
+        """ area function """
+        raise Exception("area() is not implemented")
