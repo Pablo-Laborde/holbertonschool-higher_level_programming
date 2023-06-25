cd33f400174229efbc8b2954cc66a8b534ddcc84
Pablo <p5.tacoronte@gmail.com>
2023-06-25 14:50:50 -0700

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,9 @@ from sys import argv as av
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    obj = []
 
-save_to_json_file(av[1:], "add_item.json")
-load_from_json_file("add_item.json")
+save_to_json_file(obj + av[1:], "add_item.json")
