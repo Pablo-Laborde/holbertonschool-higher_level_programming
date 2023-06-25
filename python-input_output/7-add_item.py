48662ab4dc7463a2de6086888f52b3add8b30be6
Pablo <p5.tacoronte@gmail.com>
2023-06-25 14:28:24 -0700

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,4 +7,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 save_to_json_file(av[1:], "add_item.json")
-print(load_from_json_file("add_item.json"))
+load_from_json_file("add_item.json")
