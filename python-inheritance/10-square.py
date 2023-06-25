22294be6f637235028e0961197432d86e2828ac1
Pablo <p5.tacoronte@gmail.com>
2023-06-25 03:26:43 -0700

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Ex 10 """
+
+Rec = __import__('9-rectangle').Rectangle
+
+
+class Square(Rec):
+    """ square class """
+
+    def __init__(self, size):
+        """ initialize square """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ prints a square """
+        return f"[Rectangle] {self.__size}/{self.__size}"
