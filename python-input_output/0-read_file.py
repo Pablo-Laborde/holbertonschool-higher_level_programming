ba8c330619cfa983c02132e0fa1cdbf26d0a1147
Pablo <p5.tacoronte@gmail.com>
2023-06-25 10:32:15 -0700

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Ex 0 """
+
+
+def read_file(filename=""):
+    """ read file function """
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line)
