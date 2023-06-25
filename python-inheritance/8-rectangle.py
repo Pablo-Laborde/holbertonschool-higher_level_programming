801141a4d086a2d5335f9d81484f7aba17477c75
Pablo <p5.tacoronte@gmail.com>
2023-06-25 00:52:36 -0700

    8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Ex 8 """
+
+
+class BaseGeometry():
+    """ Basegeometry class """
+    def area(self):
+        """ area function """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ int val function """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+
+    def __init__(self, width, height):
+        """ init """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        __width = width
+        __height = height
