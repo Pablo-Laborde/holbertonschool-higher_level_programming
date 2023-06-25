f4832edef0362c32ffe558d8abe4c4f47310682c
Pablo <p5.tacoronte@gmail.com>
2023-06-25 16:57:48 -0700

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Ex 10 """
+
+
+class Student():
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ initialization function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json function """
+        if type(attrs) == list:
+            my_dic = {}
+            for i in self.__dict__:
+                if i in attrs:
+                    my_dic[i] = self.__dict__.get(i)
+            return my_dic
+        return self.__dict__
