cfe1662db65e8a204be945c70017b882738178cc
Pablo <p5.tacoronte@gmail.com>
2023-06-24 19:53:49 -0700

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,7 +2,10 @@
 """ Ex 0"""
 
 
+# Function that returns a list containing attributes and methods
 def lookup(obj):
+    """ test if works """
     # print(type(dir(obj)))
     nl = dir(obj)
+    # print(isinstance(nl, list))
     return nl
