ca301bc54a89f7a034654deb4d3dc51d2a153cbf
Pablo <p5.tacoronte@gmail.com>
2023-06-30 16:04:47 -0700

    rec

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,16 @@ class Rectangle(B):
     """ Rectangle class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ initialization of rectangle """
+        """ initialization of rectangle
+        
+            variables:
+                width / height / x / y
+        """
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
 
     @property
     def width(self):
