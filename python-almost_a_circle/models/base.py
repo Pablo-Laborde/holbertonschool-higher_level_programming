d3c18b1a965062f2764177299f328898ff88d280
Pablo <p5.tacoronte@gmail.com>
2023-07-01 11:17:30 -0700

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,5 +21,5 @@ class Base():
     def to_json_string(list_dictionaries):
         """ json string """
         if list_dictionaries is None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
