0fe7d2b9b859bc3a2cc44cbcd263273cedc078c6
Pablo <p5.tacoronte@gmail.com>
2023-06-30 22:11:53 -0700

    squ

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """ returns square size """
-        return self.height
+        return self.width
     
     @size.setter
     def size(self, size):
