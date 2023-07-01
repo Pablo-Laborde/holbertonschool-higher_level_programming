2b7add56a9da08b8171eeef1bd3595e95d24d3ce
Pablo <p5.tacoronte@gmail.com>
2023-06-30 21:56:47 -0700

    squ

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Rectangle File """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ square class """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ square initialization """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str print """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
---
+++ python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square Unitest """
+
+import unittest
+from models.square import Square
+
+
+class TestMaxInt(unittest.TestCase):
+    """ class test """
+    def test_max_int(self):
+        """ test function """
+        pass
