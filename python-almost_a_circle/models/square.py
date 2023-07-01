f3b0ae305ce4768e7ee8786e70a16bca5abd5d7e
Pablo <p5.tacoronte@gmail.com>
2023-06-30 22:19:39 -0700

    squ

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,3 +24,26 @@ class Square(Rectangle):
     def size(self, size):
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """ updates the rectangle """
+        length = len(args)
+        if length > 3:
+            self.y = args[3]
+        if length > 2:
+            self.x = args[2]
+        if length > 1:
+            self.width = args[1]
+            self.height = args[1]
+        if length > 0:
+            self.id = args[0]
+        for arg in kwargs:
+            if arg == "id":
+                self.id = kwargs[arg]
+            if arg == "width" or arg == "height":
+                self.width = kwargs[arg]
+                self.height = kwargs[arg]
+            if arg == "x":
+                self.x = kwargs[arg]
+            if arg == "y":
+                self.y = kwargs[arg]
