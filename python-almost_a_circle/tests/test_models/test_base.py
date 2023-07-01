b7a08eb5e0fde2459eb39ecf6d8a84913809963d
Pablo <p5.tacoronte@gmail.com>
2023-07-01 16:48:59 -0700

    tb

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 """ Rectangle Unitest """
 
-
 import unittest
+from models.base import Base
+
+
+class TestMaxInt(unittest.TestCase):
+    """ class test """
+
+    def test_obj(self):
+        """ obj test """
+        obj1 = Base()
+        self.assertEqual(self.id, 1)
+
+        obj2 = Base()
+        self.assertEqual(self.id, 2)
+
+        obj3 = Base(10)
+        self.assertEqual(self.id, 10)
