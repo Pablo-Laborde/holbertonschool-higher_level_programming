43720cb76f0733eb62c2da2bca6e58006eda6583
Pablo <p5.tacoronte@gmail.com>
2023-07-01 20:33:15 -0700

    bs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,9 +39,6 @@ class Base():
     @classmethod
     def create(cls, **dictionary):
         """ creates an obj """
-        if cls.__name__ == 'Base':
-            dummy = cls()
-        else:
-            dummy = cls(1, 1)
-            dummy.update(**dictionary)
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
         return dummy
